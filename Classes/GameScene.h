9fccee5d00fe48277b93e2932937f2116bb00ea2
James <muzzylogic@gmail.com>
2016-01-30 01:17:03 +0000

    added classes gamescene

---
+++ Classes/GameScene.h
@@ -0,0 +1,20 @@
+#ifndef __GAMESSCENE_SCENE_H__
+#define __GAMESCENE_SCENE_H__
+
+#include "cocos2d.h"
+
+class GameScene : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(GameScene);
+};
+
+#endif // __GameScene_SCENE_H__
