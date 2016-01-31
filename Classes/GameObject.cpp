1eebc7b9a4c09abb6eccde88dda2db1a5e973266
David <davidmortonmail@gmail.com>
2016-01-31 14:08:27 +0000

    game objects now move to target and attack

--- Classes/GameObject.cpp
+++ Classes/GameObject.cpp
@@ -16,6 +16,20 @@ namespace gameobject{
 		m_Deity = deity;
 	}
 
+	void GameObject::SetTarget(GameObject *target)
+	{
+		m_pTarget = target;
+
+		// Move to target
+		auto moveAction = cocos2d::MoveTo::create(4.0f * ACTION_SPEED, m_pTarget->GetSprite()->getPosition());
+		auto easeInOut = cocos2d::EaseInOut::create(moveAction->clone(), 2.0f);
+		auto delay = cocos2d::DelayTime::create(4.0f * ACTION_SPEED);
+		auto attack = cocos2d::CallFunc::create(std::bind(&GameObject::Attack, this));
+		auto sequence = cocos2d::Sequence::create(easeInOut, delay, attack, nullptr);
+
+		m_Sprite->runAction(sequence);
+	}
+
 
 	cocos2d::Sprite *GameObject::GetSprite()
 	{
@@ -23,8 +37,8 @@ namespace gameobject{
 	}
 
 
-	void GameObject::Attack(GameObject *target)
+	void GameObject::Attack()
 	{
-		target->ApplyDamage(m_fAttackValue);
+		m_pTarget->ApplyDamage(m_fAttackValue);
 	}
 }
--- Classes/GameObject.h
+++ Classes/GameObject.h
@@ -1,6 +1,8 @@
 #ifndef GAME_OBJECT_H
 #define GAME_OBJECT_H
 
+#include <memory>
+
 #include "GameDefines.h"
 
 namespace gameobject{
@@ -9,17 +11,19 @@ namespace gameobject{
 	public:
 		virtual ~GameObject(){}
 		virtual void ApplyDamage(float damage);
-		virtual void Attack(GameObject *target);
+		virtual void Attack();
 		Deities GetDeity();
 		void SetDeity(Deities deity);
 		cocos2d::Sprite *GetSprite();
 		void SetHealth(float health);
+		void SetTarget(GameObject *target);
 		float GetHealth();
 	protected:
 		float m_fHealth;
 		Deities m_Deity;
 		float m_fAttackValue;
 		cocos2d::Sprite *m_Sprite;
+		GameObject *m_pTarget;
 	};
 
 }
